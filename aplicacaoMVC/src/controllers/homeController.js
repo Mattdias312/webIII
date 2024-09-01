88a65c06f6265bf7b1f8d92cbdbd066b55bca18e
Mattdias312 <matheusf.dias12@gmail.com>
2024-09-01 11:31:18 -0300

    documentação swagger

--- aplicacaoMVC/src/controllers/homeController.js
+++ aplicacaoMVC/src/controllers/homeController.js
@@ -10,9 +10,9 @@ exports.home = (req, res) => {
 };
 
 exports.save = (req, res) => {
-    /* #swagger.tags = ['home']
-       #swagger.summary = 'Mostra informações sobre Stray code, formulário e número de cadastros'
-       #swagger.description = 'Este endpoint mostra informações sobre Stray code, formulário para novos cadastros e número de usuários cadastrados*/
+    /* #swagger.tags = ['cadastro']
+       #swagger.summary = 'chama método para savar novo usuário'
+       #swagger.description = 'Este endpoint pega as informações do formulário para criar novo cadastro*/
     if (req.body) {
         let { nome, email } = req.body;
         listacadastros.addCadastro(nome, email);
@@ -23,6 +23,9 @@ exports.save = (req, res) => {
 };
 
 exports.excluirCadastro = (req, res) => {
+    /* #swagger.tags = ['cadastro']
+       #swagger.summary = 'chama método para excluir usuário'
+       #swagger.description = 'Este endpoint pega o email do usuário para excluir cadastro*/
     const { email } = req.params;
     listacadastros.removeCadastro(email);
     res.redirect('/home');
