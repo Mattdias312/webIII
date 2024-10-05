f74faf8c48617f46ffeae3a6c28f10f1d9832643
Mattdias312 <matheusf.dias12@gmail.com>
2024-10-05 14:28:45 -0300

    feat: update and delete

--- MVCMongo/src/controllers/projectController.js
+++ MVCMongo/src/controllers/projectController.js
@@ -32,3 +32,27 @@ exports.details = async function (req, res) {
         res.status(500).json(err);
     }
 };
+
+exports.updateProject = async function (req, res) {
+    try {
+        const updatedProject = await Project.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedProject) {
+            return res.status(404).send({ message: "Projeto não encontrado!" });
+        }
+        res.status(200).json(updatedProject);
+    } catch (err) {
+        res.status(500).json({ message: `Erro ao atualizar projeto: ${err.message}` });
+    }
+};
+
+exports.deleteProject = async function (req, res) {
+    try {
+        const deletedProject = await Project.findByIdAndDelete(req.params.id);
+        if (!deletedProject) {
+            return res.status(404).send({ message: "Projeto não encontrado!" });
+        }
+        res.status(200).send({ message: "Projeto removido com sucesso!" });
+    } catch (err) {
+        res.status(500).json({ message: `Erro ao remover projeto: ${err.message}` });
+    }
+};
--- MVCMongo/src/controllers/userController.js
+++ MVCMongo/src/controllers/userController.js
@@ -30,3 +30,27 @@ exports.details = async function (req, res) {
         res.status(500).json(err);
     }
 };
+
+exports.updateUser = async function (req, res) {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedUser) {
+            return res.status(404).send({ message: "Usuário não encontrado!" });
+        }
+        res.status(200).json(updatedUser);
+    } catch (err) {
+        res.status(500).json({ message: `Erro ao atualizar usuário: ${err.message}` });
+    }
+};
+
+exports.deleteUser = async function (req, res) {
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.params.id);
+        if (!deletedUser) {
+            return res.status(404).send({ message: "Usuário não encontrado!" });
+        }
+        res.status(200).send({ message: "Usuário removido com sucesso!" });
+    } catch (err) {
+        res.status(500).json({ message: `Erro ao remover usuário: ${err.message}` });
+    }
+};
